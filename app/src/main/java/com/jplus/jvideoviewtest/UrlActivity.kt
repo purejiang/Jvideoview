4003aae338ac5fac03e4dff9ff55e840aca2d547
JiangCodePlus <m18569290458@163.com>
2019-12-10 17:32:21 +0800

    代码优化、增加自定义示例

--- app/src/main/java/com/jplus/jvideoviewtest/UrlActivity.kt
+++ app/src/main/java/com/jplus/jvideoviewtest/UrlActivity.kt
@@ -1,43 +1,98 @@
 package com.jplus.jvideoviewtest
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
-import com.jplus.jvideoview.data.Video
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_url.*
+import java.io.UnsupportedEncodingException
+import java.net.URLDecoder
+
 
 class UrlActivity : AppCompatActivity() {
-
+    private var mIsPause = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_url)
         showDialog()
+        lv_loading.setOnClickListener {
+            mIsPause = if(mIsPause){
+                lv_loading.pause()
+                false
+            }else{
+                lv_loading.play()
+                true
+            }
+        }
+//        val text = "\n\u3000\u3000贝斯（金妮弗·古德温 Ginnifer Goodwin 饰）从小的梦想就是能够成为一名家庭主妇，如今她嫁给了罗伯特（山姆·贾格 Sam Jaeger 饰）为妻，总算是实现了理想。没想到第三者的出现将她美好的生活幻影撕成了碎片。\n                                    \n\u3000\u3000社交名媛西蒙尼（刘玉玲 饰）嫁给了非常疼爱她的卡尔（杰克·达文波特 Jack Davenport 饰），哪知道竟然在偶然之中发现卡尔竟然是一名同性恋者，在守住秘密和守住尊严之间，西蒙尼必须做出选择。\n                                    \n\u3000\u3000泰勒（柯尔比·豪威尔-巴普蒂斯特 Kirby Howell-Baptiste 饰）和埃里（瑞德·斯科特 Reid Scott 饰）正在进行一场开放式婚姻，泰勒的情人杰德（亚历珊德拉·达达里奥 Alexandra Daddario 饰）的出现让埃里开始觉得，在他们两个人里面再加一个人似乎也是个不错的主意。\n                        "
+//        tv_test.text = text
+    }
+    @Throws(UnsupportedEncodingException::class)
+    fun urlDecode(content:String, charsetName:String ="UTF-8"):String{
+        return if(content.isNotEmpty()) URLDecoder.decode(content, charsetName) else ""
     }
     private fun showDialog() {
         val view = LayoutInflater.from(this).inflate(R.layout.view_url_dialog, null, false)
         val edt = view?.findViewById<EditText>(R.id.edt_url)
         edt?.setText(
-                "https://cn5.download05.com/hls/20190804/ebfd96741e2e6e854144b2e012c30755/1564883639/index.m3u8\n" +
-                "https://cn5.download05.com/hls/20190811/fa9d03cdb6881fb674fe5d85ae55efa5/1565487765/index.m3u8\n")
+            "https://cn5.download05.com/hls/20190804/ebfd96741e2e6e854144b2e012c30755/1564883639/index.m3u8\n" +
+                    "https://cn5.download05.com/hls/20190811/fa9d03cdb6881fb674fe5d85ae55efa5/1565487765/index.m3u8\n"
+                    +"http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8"
+        )
 
         AlertDialog.Builder(this)
             .setTitle("播放地址")
             .setView(view)
             .setMessage("请输入要播放的地址：")
             .setPositiveButton("确认") { dialog, _ ->
-                edt?.text?.let{
+                edt?.text?.let {
                     dialog.dismiss()
-                    val intent =Intent(this, MainActivity::class.java)
+                    val intent = Intent(this, MainActivity::class.java)
                     intent.putExtra("urls", it.toString())
                     startActivity(intent)
                 }
             }.setNegativeButton("取消") { dialog, _ ->
                 dialog.dismiss()
-            }.show()
+            }
+            .show()
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        lineAnimator()
+    }
+
+    private fun lineAnimator() {
+//        val rootView = (this.findViewById(android.R.id.content) as ViewGroup).getChildAt(0)
+//        val width = ly_url_test.width
+//        val height = ly_url_test.height
+//        Log.d("pipa", "rootView:${rootView},height$height")
+//
+//        val animator = ValueAnimator.ofInt(500, 530, 500)
+//        animator.duration = 2000
+//        animator.addUpdateListener {
+//            val y = it.animatedValue as Int
+//            val x = width / 2
+//            moveView(lv_loading, x, y)
+//        }
+//        animator.interpolator = LinearInterpolator()
+//        animator.repeatCount = ValueAnimator.INFINITE
+//        animator.repeatMode = ValueAnimator.INFINITE
+//        animator.start()
+//        lv_loading.startAnimator()
+    }
+
+    //定义一个修改ImageView位置的方法
+    private fun moveView(view: View, rawX: Int, rawY: Int) {
+        val left = rawX + view.width / 2
+        val top = rawY - view.height
+        val width = left + view.width
+        val height = top + view.height
+        view.layout(left, top, width, height)
+    }
+
 }
