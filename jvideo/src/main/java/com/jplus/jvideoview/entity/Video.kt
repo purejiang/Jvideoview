279debc1660d2e02829b14aeca172aa488e2d3a3
HanJiang <m18569290458@163.com>
2020-04-18 16:27:20 +0800

    增加视频实体类

---
+++ jvideo/src/main/java/com/jplus/jvideoview/entity/Video.kt
@@ -0,0 +1,8 @@
+package com.jplus.jvideoview.entity
+
+/**
+ * Created by Jplus on 2020/4/18.
+ */
+data class Video(var name:String,
+                 var url:String,
+                 var progress:Long)
